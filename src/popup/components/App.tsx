b6e432be69e67270357bd80e4bf68c5c253471bc
Marcelo <49007909+verteramo@users.noreply.github.com>
2024-03-03 16:13:20 +0100

    Upload

---
+++ src/popup/components/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
---
+++ webpack.config.cjs
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const WebpackExtReloader = require("webpack-ext-reloader");
+const path = require("path");
+
+module.exports = {
+  watch: true,
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    popup: "./src/popup/index.tsx",
+    content: "./src/content/index.ts",
+    background: "./src/background/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve("static"),
+          to: path.resolve("dist"),
+        },
+      ],
+    }),
+    new WebpackExtReloader({
+      entries: {
+        extensionPage: "popup",
+        contentScript: "content",
+        background: "background",
+      },
+    }),
+  ],
+};

e5eebc1894106e3d5b125c747e339f465bdf40fa
Marcelo <49007909+verteramo@users.noreply.github.com>
2024-06-10 00:11:28 +0200

    Saving

--- src/scripts/popup.tsx
+++ src/scripts/popup.tsx
@@ -6,51 +6,11 @@
  */
 
 import { createRoot } from "react-dom/client";
-import { Card, Form } from "react-bootstrap";
-import { Github } from "react-bootstrap-icons";
-import { useTheme } from "../hooks/useTheme";
-import { Main } from "../components/Main";
+import { Popup } from "../components/Popup";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../static/popup.css";
 
-/**
- * Main card of the popup
- * @returns Card component
- */
-function Popup(): JSX.Element {
-  /** Manifest */
-  const manifest = chrome.runtime.getManifest();
-
-  /** Theme hook */
-  const [dark, toggleTheme] = useTheme();
-
-  return (
-    <Card>
-      <Card.Header>
-        <div className="d-flex justify-content-between">
-          <Card.Title>{manifest.name}</Card.Title>
-          <Form.Check
-            type="switch"
-            label="Dark"
-            title="Dark"
-            placeholder="Dark"
-            checked={dark}
-            onChange={toggleTheme}
-          />
-        </div>
-      </Card.Header>
-      <Card.Body>
-        <Main />
-      </Card.Body>
-      <Card.Footer className="d-flex justify-content-end">
-        <a title={manifest.name} href={manifest.homepage_url} target="_blank">
-          <Github />
-        </a>
-      </Card.Footer>
-    </Card>
-  );
-}
-
-// Render the popup
-createRoot(document.getElementById("root") as HTMLElement).render(<Popup />);
+const container = document.getElementById("root");
+const root = createRoot(container as HTMLElement);
+root.render(<Popup />);

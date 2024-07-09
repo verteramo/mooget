f7d0018fb6ee9fc22fc671869fe0c271665fef73
Marcelo <49007909+verteramo@users.noreply.github.com>
2024-07-10 01:10:56 +0200

    Working 2

--- src/components/Raw.tsx
+++ src/components/Raw.tsx
@@ -1,15 +1,6 @@
 import DOMPurify from 'dompurify'
 import parse from 'html-react-parser'
 
-interface IProps {
-  content: string
-}
-
-export function Raw ({ content }: IProps): JSX.Element {
-  const sanitizedContent = DOMPurify.sanitize(content)
-  const parsedContent = parse(sanitizedContent)
-
-  return (
-    <>{parsedContent}</>
-  )
+export function Raw ({ content }: { content: string }): JSX.Element {
+  return <>{parse(DOMPurify.sanitize(content))}</>
 }
